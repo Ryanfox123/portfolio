2e9ff515fcecf76d05ab6d030b2ebf4f908f1ce1
Ryan Fox <ryanfox1@hotmail.co.uk>
2024-12-30 14:08:49 +0000

    default navbar styled

--- src/app/components/NavLinks.tsx
+++ src/app/components/NavLinks.tsx
@@ -1,16 +1,27 @@
 import React from "react";
+import Image from "next/image";
+import GHicon from "../../../public/github.png";
+import LIicon from "../../../public/linkedin.png";
 
 function NavLinks() {
   return (
-    <div className="flex flex-row gap-4 mt-4">
-      <p>ig</p>
-      <img
-        src="../../public/pngwing.com.png"
-        width={50}
-        height={50}
-        alt="github icon"
-      />
-      <p>LI</p>
+    <div className="flex flex-row h-12 mt-3">
+      <div className="flex flex-col">
+        <a
+          href="https://github.com/ryanfox123"
+          target="_blank"
+          className="transition-transform opacity-80 duration-200 hover:scale-110 hover:opacity-100"
+        >
+          <Image src={GHicon} width={60} height={60} alt="github icon" />
+        </a>
+      </div>
+      <a
+        href="https://www.linkedin.com/in/ryan-fox-7708102b1/"
+        target="_blank"
+        className="ml-3 transition-transform opacity-80 duration-200 hover:scale-110 hover:opacity-100"
+      >
+        <Image src={LIicon} width={60} height={60} alt="linkedin icon" />
+      </a>
     </div>
   );
 }
--- src/app/components/NavMenu.tsx
+++ src/app/components/NavMenu.tsx
@@ -34,12 +34,12 @@ const NavMenu: React.FC = () => {
       <div ref={rfRef}>
         <p
           className={`border-2 p-3 font-extrabold tracking-widest text-4xl transition-transform duration-500 ease-in-out text-lapis border-teal bg-white overflow-hidden relative ${
-            isNear ? "scale-110 shadow-lg" : ""
+            isNear ? "scale-105 shadow-lg" : ""
           }`}
         >
           <span
             className={`absolute inset-0 bg-slate-200 ${
-              isNear ? "translate-x-0" : "-translate-x-32"
+              isNear ? "translate-x-0" : "-translate-x-20"
             } group-hover:translate-y-0 transition-transform duration-500 ease-in-out`}
           ></span>
           <span className="relative z-10">RF</span>
@@ -48,12 +48,12 @@ const NavMenu: React.FC = () => {
       <div className="relative">
         <p
           onClick={() => console.log("clicked")}
-          className={`m-auto pt-4 text-lapis transition-all text-2xl duration-500 ease-in-out hover:text-slate-500`}
+          className={`m-auto pt-4 text-lapis transition-all text-2xl duration-500 ease-in-out`}
         >
           Menu
         </p>
         <span
-          className={`absolute left-0 bottom-6 h-1 bg-slate-200 transition-all duration-1000 ease-in-out ${
+          className={`absolute left-0 bottom-3 h-1 bg-slate-200 transition-all duration-1000 ease-in-out ${
             isNear ? "w-full" : "w-0"
           }`}
         ></span>
