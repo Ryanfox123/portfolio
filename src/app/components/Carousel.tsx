526f994c2a0462c6c295682f2c51e6b394e45e69
Ryan Fox <ryanfox1@hotmail.co.uk>
2025-03-24 18:45:34 +0000

    fixes

--- src/app/components/Carousel.tsx
+++ src/app/components/Carousel.tsx
@@ -1,7 +1,6 @@
 "use client";
 import React, { useEffect, useState } from "react";
 import Image from "next/image";
-import { image } from "framer-motion/client";
 
 const images = [
   { src: "/ncgroup.png", alt: "Northcoders group picture" },
