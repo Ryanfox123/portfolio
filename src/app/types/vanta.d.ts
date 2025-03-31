ae066c8d88c424ce22ec0b18729349382f6e79e4
Ryan Fox <ryanfox1@hotmail.co.uk>
2025-03-31 21:51:49 +0100

    fixed types 2

--- src/app/types/vanta.d.ts
+++ src/app/types/vanta.d.ts
@@ -1,5 +1,22 @@
 declare module "vanta/dist/vanta.net.min" {
   import { Base } from "vanta/types/base";
 
-  export default function NET(options: any): Base;
+  interface VantaNetOptions {
+    el: HTMLElement;
+    THREE?: any;
+    mouseControls?: boolean;
+    touchControls?: boolean;
+    gyroControls?: boolean;
+    minHeight?: number;
+    minWidth?: number;
+    scale?: number;
+    scaleMobile?: number;
+    color?: number;
+    backgroundColor?: number;
+    points?: number;
+    maxDistance?: number;
+    spacing?: number;
+  }
+
+  export default function NET(options: VantaNetOptions): Base;
 }
