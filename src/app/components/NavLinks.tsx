971c423a0a7a4e4bda0c2122d9c8354211c3491d
Ryan Fox <ryanfox1@hotmail.co.uk>
2025-07-31 22:16:09 +0100

    responsive nav bar styling for page scroll location

--- src/app/components/NavLinks.tsx
+++ src/app/components/NavLinks.tsx
@@ -1,28 +1,40 @@
-import React from "react";
+"use client";
+import React, { useState, useEffect } from "react";
 import { motion } from "framer-motion";
 
 export default function NavLinks() {
+  const [scrollY, setScrollY] = useState(0);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setScrollY(window.scrollY);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
   return (
     <div className="flex flex-row h-12  justify-between">
       <div>
         <ul className="flex flex-row gap-20">
           {[
-            { name: "Projects", yLocation: 650 },
-            { name: "About Me", yLocation: 1400 },
-            { name: "Links", yLocation: 2000 },
+            { name: "Projects", yLocationStart: 650, yLocationEnd: 1399 },
+            { name: "About Me", yLocationStart: 1400, yLocationEnd: 1999 },
+            { name: "Links", yLocationStart: 2000, yLocationEnd: 2500 },
           ].map((item, index) => (
             <li
               key={index}
               onClick={() => {
                 scrollTo({
-                  top: item.yLocation,
+                  top: item.yLocationStart,
                   behavior: "smooth",
                 });
               }}
             >
               <motion.div
-                initial={{ opacity: 0, y: 0 }}
-                animate={{ opacity: 1, y: 0 }}
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
                 transition={{ delay: index * 0.2, duration: 0.5 }}
                 className="relative group cursor-pointer px-4 py-2 flex justify-center items-center"
                 whileHover="hover"
@@ -31,6 +43,12 @@ export default function NavLinks() {
                 <motion.div
                   initial={{ height: "20%" }}
                   variants={{ hover: { height: "40%" } }}
+                  animate={
+                    scrollY >= item.yLocationStart &&
+                    scrollY <= item.yLocationEnd
+                      ? { height: "40%", opacity: 0.6 }
+                      : { height: "20%", opacity: 0.3 }
+                  }
                   transition={{ duration: 0.3, ease: "easeOut" }}
                   className="absolute bottom-3 left-0 w-full bg-teal opacity-30 group-hover:opacity-60 pointer-events-none"
                 />
